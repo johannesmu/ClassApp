13d9827249eadacc7917c14087301e66fb98743e
johannes <jmuljana@gmail.com>
2020-07-27 16:49:48 +1000

    changing to class component

--- App.js
+++ App.js
@@ -1,23 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 // custom component
 import { TextBox } from './components/TextBox';
 import { ClickCounter } from './components/ClickCounter';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.greeting}>Hello World!</Text>
-      <Button title="Click me" color="blue"/>
-      <StatusBar style="auto" />
-      <TextBox color="green" size={24} text="Custom Component" />
-      <TextBox color="purple" size={18} text="Custom Text 2" />
-      {/* click counter component */}
-      <ClickCounter/>
-      <ClickCounter/>
-    </View>
-  );
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.greeting}>Hello World!</Text>
+//       <Button title="Click me" color="blue"/>
+//       <StatusBar style="auto" />
+//       <TextBox color="green" size={24} text="Custom Component" />
+//       <TextBox color="purple" size={18} text="Custom Text 2" />
+//       {/* click counter component */}
+//       <ClickCounter/>
+//       <ClickCounter/>
+//     </View>
+//   );
+// }
+
+export default class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      secret: 0
+    }
+  }
+  render() {
+    return (
+      <View>
+        <Text>Guessing Game</Text>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
